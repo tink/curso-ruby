1e116874cbc96feeb58a0fbe83eabdf30d5e8396
Anderson dias <andersondaraujo@gmail.com>
2010-12-30 10:44:46 -0300

    Exemplos de manipulação de números em Ruby

---
+++ 01_numeros.rb
@@ -0,0 +1,45 @@
+## Inteiros
+5
+0
+-205
+
+9999.class
+9999999999999999999999999.class # Conversão automática de tipos
+
+## Float
+54.321
+0.001
+-205.3884
+0.0
+
+## Aritmética simples
+1 + 2
+2 * 3
+5 - 8
+9 / 2 # Atenção: Divisões com inteiros sempre retorna inteiro
+
+1.0 + 2.0
+2.0 * 3.0
+5.0 - 8.0
+9.0 / 2.0
+
+5 * (12-8) + -15
+98 + (59872 / (13*8)) * -52
+
+## Métodos básicos
+1.to_f # Conversão para float
+1.1.to_i # Conversão para inteiro
+1.9.to_i # Conversão para inteiro sempre arredonda para baixo
+
+56.1.ceil # Arrendondamento para cima
+56.4.round # Arrendondamento por proximidade
+57.9.round
+57.9.floor # Arredondamento para baixo
+
+3.even? # é par?
+3.odd?  # é ímpar?
+3.zero? # é zero?
+0.zero?
+
+(1.655).abs # Valor absoluto de um número (módulo de um número)
+(-9.98798).abs
